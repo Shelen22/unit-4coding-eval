d02d03bc17ceda147c4d64ad549d25081ddb11c1
Shailendra Singh <shailendrauday5@gmail.com>
2021-12-13 14:41:56 +0530

    multer

--- unit-4 eval/src/models/user.model.js
+++ unit-4 eval/src/models/user.model.js
@@ -3,5 +3,15 @@ const {schema, model} = require('mongoose')
 
 const userSchema = new schema({
    
-    Name: {type: String, required: true}
-})
+    name: {type: String, required: true},
+    email : {type: String, required: true,unique: true},
+    password : {type: String, required: true, unique: true},
+    profile_pic: {type:String, required: true},
+    roles: {type:String, required: true}
+},
+{
+  versionKey:false,
+  timestamps: true
+});
+
+module.exports = model("user", userSchema);
